7bcb9c088e403e01226f46dc9473d199668de177
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-02-28 16:57:01 +0100

    "Exercice RechercheMax"

---
+++ src/fr/algorithmie/RechercheMax.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+public class RechercheMax {
+
+	public static void main(String[] args) {
+		
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		
+		/* Recherche du plus grand élément de array et affichage de celui-ci. */
+		int max = -20; // je prends un nombre pas dans array et plus petit que tous.
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		System.out.println("max = " + max);
+
+	}
+
+}
