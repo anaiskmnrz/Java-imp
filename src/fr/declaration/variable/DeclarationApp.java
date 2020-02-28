b0467e6c427211c9d9b9af60e2c8c440f9e34bc0
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-02-28 12:27:53 +0100

    "fin TP déclaration de variables"

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,31 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 3;
+		double d = 3456648486.2;
+		float f = 5897.12f;
+		short s = 129;
+		byte b = 127;
+		char c = 'a';
+		boolean B = true;
+		long l = 15684166554865465L;
+	
+		System.out.println("int = " + i);
+		System.out.println("double = "+ d);
+		System.out.println("float = " + f);
+		System.out.println("short = " + s);
+		System.out.println("byte = " + b);
+		System.out.println("char = " + c);
+		System.out.println("boolean = " + B);
+		System.out.println("long = " + l + "\n");
+		
+		String randomString = "Voici le résultat d'un calcul:\n1+5=6";
+		
+		System.out.println(randomString);
+	}
+
+}
